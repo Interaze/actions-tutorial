b109a57b41b947c5e30558c0e11c8078dee24546
Christopher Scully <scullycphone@gmail.com>
2021-01-16 14:08:48 -0600

    fix: some feature

--- release.config.js
+++ release.config.js
@@ -2,8 +2,8 @@ module.exports = {
   branches: "master",
   repositoryUrl: "https://github.com/Interaze/actions-tutorial",
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/github'
-  ]
-}
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
